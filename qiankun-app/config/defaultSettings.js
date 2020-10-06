8b123329c2144e14e42bce5b070d4ba597d00c14
黄小标 <huangxb0512@gmail.com>
2020-10-06 21:42:49 +0800

    first commit

---
+++ qiankun-app/config/defaultSettings.js
@@ -0,0 +1,17 @@
+const proSettings = {
+  "navTheme": "dark",
+  "primaryColor": "#1890ff",
+  "layout": "side",
+  "contentWidth": "Fluid",
+  "fixedHeader": false,
+  "fixSiderbar": true,
+  "menu": {
+    "locale": true
+  },
+  "title": "Ant Design Pro",
+  "pwa": false,
+  "iconfontUrl": "",
+  "headerRender": false,
+  "footerRender": false
+};
+export default proSettings;
---
+++ qiankun-main/config/defaultSettings.js
@@ -0,0 +1,17 @@
+const proSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'topmenu',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  fixSiderbar: true,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+};
+export default proSettings;
